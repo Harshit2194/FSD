a70834068732b5f700b6e9db07b454c9881cda9c
Harshit2194 <patelharshit156@gmail.com>
2025-11-06 14:25:27 +0530

    card creation

---
+++ React/firstProj/src/Cars.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Card.css';
+
+function Cars(props){
+    return(
+        <div className="card">
+            <img src={props.images} alt= {props.title} className="card-imgae" />
+            <h3 className = "card-title">{props.title}</h3>
+            <p className = "card-description">{<props.description}</p>
+        </div>
+    );
+}
+export default Cars;
