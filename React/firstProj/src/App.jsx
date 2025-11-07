c866cbb34da7151ab1078c381d4184346d19b9e2
Harshit2194 <patelharshit156@gmail.com>
2025-11-07 11:49:20 +0530

    Basic Card creation 7 NOV

--- React/firstProj/src/App.jsx
+++ React/firstProj/src/App.jsx
@@ -2,18 +2,40 @@ import { useState } from 'react'
 import './App.css'
 import Car from './components/Car';
 import Bike from './components/Bike';
+import Card from './Card';
+function App(){
+  return(
+      <div className="card-container">
+        <Card 
+        image = "./public/imp.png"
+        title = "React Basics"
+        description="Learn about compoments ,props, and state."
+      />
 
-function App() {
+      <Card 
+        image = "./public/imp.png"
+        title = "React Basics"
+        description="Learn about compoments ,props, and state."
+      />
 
-  return (
-    <>
-      <Car/>
-      <Bike/>
-      <Bike/>
-      <Bike/>
-      <Bike/>
-    </>
-  )
+      <Card 
+        image = "./public/imp.png"
+        title = "React Basics"
+        description="Learn about compoments ,props, and state."
+      />
+
+      <Card 
+        image = "./public/imp.png"
+        title = "React Basics"
+        description="Learn about compoments ,props, and state."
+      />
+
+      
+
+      
+      </div>
+  );
 }
+
 
 export default App
--- React/firstProj/src/Card.jsx
+++ React/firstProj/src/Card.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import './Card.css';
 
-function Cars(props){
+function Card(props){
     return(
         <div className="card">
-            <img src={props.images} alt= {props.title} className="card-imgae" />
+            <img src={props.image} alt= {props.title} className="card-imgae" />
             <h3 className = "card-title">{props.title}</h3>
-            <p className = "card-description">{<props.description}</p>
+            <p className = "card-description">{props.description}</p> 
         </div>
     );
 }
